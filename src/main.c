29cbe6c0487555690414381c2d8cb3a30478eebf
Simon Bertling <simon.bertling@gmx.de>
2018-03-07 00:42:01 +0900

    added sample code to interface CPP code (in this case templates)

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include "adc.h"
 #include "buffer.h"
 
+#include "template.h"
+
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -25,9 +27,16 @@ int main(void)
 
 	buffer_u8_init(&buf);
 
-	uint8_t i = 0;
+	int i = 0;
+	char c = 0;
 	while(1)
 	{
+		i += 100;
+		c += 10;
+		UART_putd(test_template_int(i));
+		UART_puts(", ");
+		UART_putd(test_template_char(c));
+		UART_puts("\n\r");
 		// UART_putd_32(timer0_ovfl_interrupt_call_counter);
 		// UART_puts("\t");
 		// UART_putd_32(timer0_compa_interrupt_call_counter);
@@ -42,31 +51,31 @@ int main(void)
 		// UART_putd(adc_val.val);
 		// UART_puts("\t");
 		// UART_putd(ADC_single_shot(ADC_MUX_ADC2));
-		i += 1;
-		uint8_t usage = 0;
-		UART_puts("i ");
-		UART_putd(i);
-		UART_puts(",");
-		UART_putd(buffer_u8_write(&buf, i));
-		UART_puts(",");
-		UART_putd(buffer_u8_usage(&buf, &usage));
-		UART_puts(",usage ");
-		UART_putd(usage);
-		if(usage == BUFFER_SIZE_MAX)
-		{
-			UART_puts(", values: ");
-			while(usage > 0)
-			{
-				uint8_t val = 0;
-				buffer_u8_read(&buf,&val);
-				buffer_u8_usage(&buf, &usage);
-				UART_putd(val);
-				UART_puts(",");
-				PORTD |= (1<<PD3);
-			}
-		}
-		PORTD &= ~(1<<PD3);
-		UART_puts("\n\r");
+		// i += 1;
+		// uint8_t usage = 0;
+		// UART_puts("i ");
+		// UART_putd(i);
+		// UART_puts(",");
+		// UART_putd(buffer_u8_write(&buf, i));
+		// UART_puts(",");
+		// UART_putd(buffer_u8_usage(&buf, &usage));
+		// UART_puts(",usage ");
+		// UART_putd(usage);
+		// if(usage == BUFFER_SIZE_MAX)
+		// {
+		// 	UART_puts(", values: ");
+		// 	while(usage > 0)
+		// 	{
+		// 		uint8_t val = 0;
+		// 		buffer_u8_read(&buf,&val);
+		// 		buffer_u8_usage(&buf, &usage);
+		// 		UART_putd(val);
+		// 		UART_puts(",");
+		// 		PORTD |= (1<<PD3);
+		// 	}
+		// }
+		// PORTD &= ~(1<<PD3);
+		// UART_puts("\n\r");
 		timer0_wait_ms_blocking(100);
 		//PORTD ^= (1<<PD2);
 		//i = i+10;
